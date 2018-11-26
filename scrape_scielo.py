48f05c3fb1415657c15d28271b0c3e42baf8ece0
Neylson Crepalde <neylsoncrepalde@gmail.com>
2018-11-26 01:01:36 -0200

    clean input data and export authors and affiliation data

--- scrape_scielo.py
+++ scrape_scielo.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-from urllib.request import urlopen
+from urllib.request import urlopen, HTTPError
+from urllib.error import URLError
 from bs4 import BeautifulSoup
+import pandas as pd
 import csv
 import time
 import re
+import os
 
 
 class ScrapeScielo:
@@ -66,31 +69,134 @@ class ScrapeScielo:
         print("Done")
         print("Scraping of the articles took {} seconds.".format(round(end-begin, 3)))
 
-    def scrape_xml(self):
-        print("Scraping xml links for the articles...")
+
+    def scrape_content(self):
+        #Abrindo os arquivos
+        saida = open('articles_content.csv','w')
+        export = csv.writer(saida)
+        export.writerow(['title','year','journal', 'authors', 'affs',
+                         'keywords'])
+
+        #regex = re.compile("\xa0")
+        #rent = re.compile("[\n|\t]")
+        #relink = re.compile("\[ Link\] ")
+
         begin = time.time()
 
-        saida = open("links_xml.csv", "w")
-        export = csv.writer(saida)
-        
         arquivo = open('artigos.csv')
-        artigos = arquivo.readlines()
+        links_artigos = arquivo.readlines()
         newline = re.compile("\n")
-        artigos = [newline.sub("", i) for i in artigos]
-        
-        for url in artigos:
-            pagina = urlopen(url)
-            pagina = BeautifulSoup(pagina, "lxml")
-            
-            for i in pagina.findAll("a", {"target":"xml"}):
-                pagina_xml = i.get('href')
-                export.writerow([pagina_xml])
-        
-        end = time.time()
+        links_artigos = [newline.sub("", i) for i in links_artigos]
+        links_artigos = list(set(links_artigos))
+
+        url_broken = []
+        contador = 1
+        for url in links_artigos:
+            if contador % 10 == 0:
+                print("Scraping {} of {} articles...".format(contador, len(links_artigos)))
+
+            try:
+                page = urlopen(url)
+                page = BeautifulSoup(page, "lxml")
+            except URLError:
+                contador += 1
+                url_broken.append(url)
+                continue
+
+            journal = page.find("meta", {"name": "citation_journal_title"}).get("content")
+            title = page.find("meta", {"name": "citation_title"}).get("content")
+            year = page.find("meta", {"name": "citation_date"}).get("content")[-4:]
+            autores = [i.get("content") for i in page.findAll("meta", {"name": "citation_author"})]
+            autores = list(set(autores))
+            affs = [i.get("content") for i in page.findAll("meta", {"name": "citation_author_institution"})]
+            affs = list(set(affs))
+
+            abstract = page.find("meta", {"name": "citation_abstract_html_url"}).get("content")
+
+            try:
+                pageabs = urlopen(abstract)
+                pageabs = BeautifulSoup(pageabs, "lxml")
+                keys = [i.get_text() for i in pageabs.find("div", {"class": "index,pt"}).findAll("p")[4]]
+            except HTTPError:
+                keys = [""]
+
+            # Escrevendo os dados
+            export.writerow([title, year, journal, autores, affs, keys])
+
+            contador += 1
+
         saida.close()
-        print("Done")
-        print("Scraping of the xml links took {} seconds.".format(round(end-begin, 3)))
-        
-    def scrape_content(self):
-        pass
-        
+
+        end = time.time()
+
+        print('Done')
+        print("Total of {} broken URLs".format(len(url_broken)))
+        print("Scraping of the article content took {} seconds.".format(round(end-begin, 3)))
+
+    def clean_data(self):
+        print("Cleaning data and exporting datasets.....")
+        bd = pd.read_csv("articles_content.csv")
+        autores = bd.authors
+        rebegin = re.compile("\[")
+        reend = re.compile("\]")
+        regex = re.compile("', '")
+        reaspas = re.compile("'")
+        autores = [rebegin.sub("", i) for i in autores]
+        autores = [reend.sub("", i) for i in autores]
+        autoresnew = [regex.split(i) for i in autores]
+
+        for i in range(len(autoresnew)):
+            for j in range(len(autoresnew[i])):
+                autoresnew[i][j] = reaspas.sub("", autoresnew[i][j])
+
+        # Verificando o tamanho máximo no banco
+        max_autores = max([len(i) for i in autoresnew])
+
+        # Cria max_autores novas variáveis no banco
+        for i in range(max_autores):
+            bd['autor' + str(i)] = ""
+
+        # Coloca os dados no lugar certo
+        for i in range(len(autoresnew)):
+            tamanho = len(autoresnew[i])
+            for j in range(tamanho):
+                bd.iloc[i, j+6] = autoresnew[i][j]
+        bd_autores = bd.drop(['authors', 'keywords'], axis=1)
+
+        # Faz a mesma limpeza para afiliaçoes
+        afiliacoes = bd.affs
+        afiliacoes = [rebegin.sub("", i) for i in afiliacoes]
+        afiliacoes = [reend.sub("", i) for i in afiliacoes]
+        afiliacoesnew = [regex.split(i) for i in afiliacoes]
+        for i in range(len(afiliacoesnew)):
+            for j in range(len(afiliacoesnew[i])):
+                afiliacoesnew[i][j] = reaspas.sub("", afiliacoesnew[i][j])
+
+        # Verificando o tamanho máximo no banco
+        max_afiliacoes = max([len(i) for i in afiliacoesnew])
+
+        # Cria max_autores novas variáveis no banco
+        for i in range(max_afiliacoes):
+            bd['inst' + str(i)] = ""
+
+        # Coloca os dados no lugar certo
+        for i in range(len(afiliacoesnew)):
+            tamanho = len(afiliacoesnew[i])
+            for j in range(tamanho):
+                bd.iloc[i, j+6] = afiliacoesnew[i][j]
+        bd_afiliacoes = bd.drop(['affs', 'keywords'], axis=1)
+
+        # Exportando os bancos
+        bd_autores.to_csv("scielo_autores.csv", index=False)
+        bd_afiliacoes.to_csv("scielo_afiliacoes.csv", index=False)
+
+        print('Removing junk data...')
+        os.remove(['edicoes.csv', 'artigos.csv', 'articles_content.csv'])
+
+        print('Done!!!')
+
+    def get_all_articles(self):
+        self.scrape_editions()
+        self.scrape_articles()
+        self.scrape_content()
+        self.clean_data()
